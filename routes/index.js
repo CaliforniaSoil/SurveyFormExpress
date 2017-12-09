f369d21e36f04873d22c507fad22431e7029dc67
Admin <admin@admins-mbp.attlocal.net>
2017-12-09 06:00:14 -0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
-module.exports = function Route(app){
+module.exports = function Route(app, server){
+	// this gets the socket.io module
+	var io = require('socket.io').listen(server) 
 	// root route to render the index.ejs view
 	app.get('/', function(req, res) {
-	 res.render("index");
-	});
-	// post route for adding a data from a survey
-	app.post('/result', function(req, res) {
-		const submittedInfo = {
-			name: req.body.name,
-			dojoLocation: req.body.dojoLocation,
-			favoriteLanguage: req.body.favoriteLanguage,
-			comment: req.body.comment
-    };
-
-	 	res.render("result", { userData: submittedInfo });
-	});
+		res.render("index");
+	})
+	//listen to connection even from the client side
+	io.sockets.on('connection', function (socket){
+		//server listens to "posting_form" event
+	 	socket.on("posting_form", function (data){
+	 		//generate a random number
+	 		var random_number = Math.floor((Math.random() * 1000) + 1);
+		  //will emit the data to the client
+		  socket.emit('updated_message', {response: data}); 
+			socket.emit('random_number', {response: random_number}); 
+		})
+	})
 };
